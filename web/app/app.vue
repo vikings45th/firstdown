a7f036eb8503764cd5bb19a6ca5576962144afd4
banahihihi <songxiafengzhi@gmail.com>
2026-02-15 01:58:13 +0900

    update

--- web/app/app.vue
+++ web/app/app.vue
@@ -1,15 +1,27 @@
 <script setup lang="ts">
-  const runtimeConfig = useRuntimeConfig()
-  // Dynamic Library Importを使用してGoogle Maps APIを読み込む
-  useHead({
-    script: [
-      {
-        src: `https://maps.googleapis.com/maps/api/js?key=${runtimeConfig.public.googleMapsApiKey}&libraries=maps,marker&v=weekly`,
-        defer: true,
-        key: 'google-maps-api' // 重複を防ぐためのキー
-      }
-    ]
-  })
+const runtimeConfig = useRuntimeConfig();
+// Dynamic Library Importを使用してGoogle Maps APIを読み込む
+useHead({
+  htmlAttrs: {
+    lang: "ja",
+  },
+  title: "てくてく | 今の気分に合う散歩ルートを一つだけ提案します",
+  meta: [
+    { name: "viewport", content: "width=device-width, initial-scale=1" },
+    {
+      name: "description",
+      content:
+        "チャットでいくつか答えるだけであなたの気分に寄り添った散歩コースを提案します",
+    },
+  ],
+  script: [
+    {
+      src: `https://maps.googleapis.com/maps/api/js?key=${runtimeConfig.public.googleMapsApiKey}&libraries=maps,marker&v=weekly`,
+      defer: true,
+      key: "google-maps-api", // 重複を防ぐためのキー
+    },
+  ],
+});
 </script>
 
 <template>
